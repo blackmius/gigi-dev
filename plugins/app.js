8f4b7388f02bd9d35589977e44affc7f6f221d43
Agigi Developer <fedya.kryutchenko@gmail.com>
2017-03-29 14:53:45 +0300

    App plugin has been improved and minifyed

--- plugins/app.js
+++ plugins/app.js
@@ -1,20 +1,12 @@
-gi.app = function(name) {
-	var map = function(el, fn) { var ret = []; for(var i = 0; i < el.length; i++) ret[i] = fn(el[i]); return ret; }
-	var body = function() {
-		return gi("." + self.name + "-pages",
-			map(self.pages, function(page) {
-				return gi("." + self.name + "-page", page.name == self.page? page.content: "");
-			})
-		);
-	};
+gi.app = function(app) {
+	var container = app.container || document.body;
+	var body = function() { app.body(app.vars); };
 	
 	var self = {
-		update: function() { gi.vdom.update(bodyDom, body); }, name: name || "app",
-		pages: [], page: null, state: {/* GLOBAL VARIABLES HERE */}, pageClass: "app-page",
-		addPage: function(name, pg) { self.pages.push({name: name, content: pg}); },
-		showPage: function(name) { self.page = name; self.update(); }, content: body
+		update: function() { gi.vdom.update(bodyDom, body); },
+		pages: [], page: null, vars: app.vars
 	};
-	var bodyDom = gi.vdom.append(document.body, body);
+	var bodyDom = gi.vdom.append(container, body);
 	gi.ready(self.update);
 	return self;
 };
