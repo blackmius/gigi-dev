a414f3b0ab685553dab74231a3f54397af8f317a
Agigi Developer <fedya.kryutchenko@gmail.com>
2017-03-29 15:46:48 +0300

    BUGFIX: app.js

--- plugins/app.js
+++ plugins/app.js
@@ -1,12 +1,14 @@
 gi.app = function(app) {
 	var container = app.container || document.body;
 	var body = function() { return app.body(app.vars); };
-	
+	var bodyDom;
 	var self = {
 		update: function() { gi.vdom.update(bodyDom, body); },
-		pages: [], page: null, vars: app.vars
+		pages: [], page: null, vars: app.vars,
+		init: function() {
+	        bodyDom = gi.vdom.append(container, body);
+	        gi.ready(self.update);
+        }
 	};
-	var bodyDom = gi.vdom.append(container, body);
-	gi.ready(self.update);
 	return self;
 };
