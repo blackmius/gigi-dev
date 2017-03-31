519d94fe9aa0286d0ca741415b5967fa3a440759
Agigi Developer <fedya.kryutchenko@gmail.com>
2017-04-01 01:52:48 +0300

    REWRITE: gi.selector

--- plugins/selector.js
+++ plugins/selector.js
@@ -1,1 +1,7 @@
-gi.selector = function(list, selected) { return function() { return list[selected]; }; };
+gi.selector = function(list, _selected) {
+    var self = {
+        select: function(name) { _selected = name },
+        get value() { return list[_selected]; }
+    };
+    return self;
+};
